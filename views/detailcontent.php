df430a693509fd08eb937f359c2d1ff264cdcd4e
Wahyu Aprilliandhika <wahyuandhika910@gmail.com>
2025-01-02 10:38:46 +0700

    update

--- views/detailcontent.php
+++ views/detailcontent.php
@@ -33,7 +33,7 @@ if (isset($_GET['slug'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
+    <title><?= htmlspecialchars($article['title']); ?></title>
 
     <!-- font awesome cdn link  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
